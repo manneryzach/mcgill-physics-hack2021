46d411b71eafba3fbc1b5ca2669d60a554a382ce
Zachary Mann <zachary.et.mann@gmail.com>
2021-11-06 15:34:23 -0400

    m

--- fem_solver.py
+++ fem_solver.py
@@ -8,7 +8,41 @@ def generate_mesh(rects, h, K):
         temp_rect = gf.MesherObject("rectangle", rects[i][0], rects[i][1])
         union = gf.MesherObject('union', union, temp_rect)
 
+    # union of all the rectangles
     mesh = gf.Mesh('generate', union, h, K)
+
+    # boundaries
+    outer_faces = mesh.outer_faces()
+    mesh.set_region(1, outer_faces)
 
     return mesh
 
+def model(mesh, wavenum):
+    # Separation into finite elements
+    mfu = gf.MeshFem(mesh, 1)
+
+    # Integration method
+    elements_degree = 2
+    mfu.set_classical_fem(elements_degree)
+
+    mim = gf.MeshIm(mesh, pow(elements_degree, 2))
+
+    # Model
+    md = gf.Model("real")
+    md.add_fem_variable("u", mfu)
+
+    # Laplacian term of Helmholtz equation
+    md.add_Laplacian_brick(mim, "u")
+
+    # Eigenvalue term of the equation
+    md.add_Helmholtz_brick(mim, "u", wavenum)
+
+    # Dirichlet boundary conditions
+    md.add_Dirichlet_condition_with_multipliers(mim, "u", (elements_degree - 1), 1)
+
+    # Solve the model
+    md.solve()
+
+    U = md.variable("u")
+
+    return U
--- fem_solver_tester.py
+++ fem_solver_tester.py
@@ -9,18 +9,13 @@ from fem_solver import *
 rects = [ [ [0.,0.], [1.,1.] ], [ [0.,1.], [1.,2.] ] ]
 
 mesh1 = generate_mesh(rects, 0.1, 1)
+mfu = gf.MeshFem(mesh1, 1)
+
+U = model(mesh1, 2.)
+print(U)
 
 sl = gf.Slice(("none",), mesh1, 1)
-sl.export_to_vtk('sl.vtk', "ascii")
+sl.export_to_vtk('sl.vtk', "ascii", mfu, U, "U")
 
-# Display
-display = Display(visible=0, size=(1280, 1024))
-display.start()
-p = pv.Plotter()
-m = pv.read("./sl.vtk")
-p.add_mesh(m, show_edges=True)
-pts = m.points
-p.show(window_size=[512, 384], cpos="xy")
-display.stop()
 
 
